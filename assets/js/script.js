e7f25913619823f9c93478bb9ff5f2e27374de1a
SamuelMasters <samuelmastersdev@gmail.com>
2022-06-11 15:36:08 +0000

    Add displayStatus function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,16 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Status: " + data.status_code;
+    document.getElementById("results-content").innerText = `Your key is valid until
+    ${data.expiry}`;
+
+    resultsModal.show();
 }
